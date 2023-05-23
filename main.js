7ab30eb2199295ba817a1a4a7747c38cd46d74f0
sh <reki1.reki2.reki3@gmail.com>
2023-05-23 12:02:10 +0900

    モーダルウィンドウ

--- main.js
+++ main.js
@@ -16,3 +16,22 @@
     document.querySelector("ul").appendChild(li);
   });
 }
+{
+  const open = document.getElementById("open");
+  const back = document.getElementById("back");
+  const modal = document.getElementById("modal");
+  const close = document.getElementById("close");
+  open.addEventListener("click", () => {
+    modal.classList.remove("s1");
+    back.classList.remove("s1");
+  });
+  close.addEventListener("click", () => {
+    modal.classList.add("s1");
+    back.classList.add("s1");
+  });
+  back.addEventListener("click", () => {
+    modal.classList.add("s1");
+    back.classList.add("s1");
+  });
+
+  }
