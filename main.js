56040fbc215d86643cbe161ce109483816a6e355
sh <reki1.reki2.reki3@gmail.com>
2023-05-23 09:32:23 +0900

    5.23

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-'use strict'
+'use strict';
 
 {
-  
+  document.querySelector("button").addEventListener("click", () => {
+    const ones = document.querySelectorAll("input");
+    const two = [];
+
+    ones.forEach(one => {
+    if (one.checked === true) {
+      two.push(one.value);
+    }
+    });
+
+    const li = document.createElement("li");
+    li.textContent = two;
+    document.querySelector("ul").appendChild(li);
+  });
 }
